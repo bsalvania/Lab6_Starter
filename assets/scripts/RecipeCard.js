a0231f0807a4b2e0baa18d962ab62570e2c1b37a
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-10 21:58:46 -0700

    Finished A6-8

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -121,12 +121,31 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+    let articleEl = this.shadowRoot.querySelector('articleEl');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+    articleEl.innerHTML = `
+    <img src="${data.imgSrc}"
+      alt="${data.imgAlt}">
+    <p class="title">
+      <a href="${data.titleLnk}">${data.titleTxt}</a>
+    </p>
+    <p class="organization">${data.organization}</p>
+    <div class="rating">
+      <span>${data.rating}</span>
+      <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+      <span>${data.numRatings}</span>
+    </div>
+    <time>${data.lengthTime}</time>
+    <p class="ingredients>
+      ${data.ingredients}
+    </p>
+    `;
   }
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define('recipe-card', RecipeCard);
